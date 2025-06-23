0186ea42c3ceea535b1f102b9690362f446206cf
Ashwin Kanitkar <ashwinkanitkar@outlook.com>
2025-06-24 02:10:27 +1000

    Added more indicators with legends. Option to resample data into larger timeframe and allow users to enter chosen csv file.

--- BackTest.py/strategy1.py
+++ BackTest.py/strategy1.py
@@ -1,27 +1,38 @@
 from backtesting import Backtest, Strategy
 from backtesting.lib import crossover
+from backtesting.lib import resample_apply
 
 import pandas as pd
 import pandas_ta as ta # Using panda's TA lib
-import numpy as np
-
 from backtesting.test import GOOG #Google test data between 08/2004 -> 05/2009
 
-
-
-# calculate indicators and add them as columns
-GOOG['EMA9'] = GOOG.ta.ema(length=9)
-GOOG['EMA21'] = GOOG.ta.ema(length=21)
-
+# delcaring all indicator functions to be used during simulation
+def EMA9(x):
+    return ta.ema(pd.Series(x), length=9).to_numpy()
+def EMA21(x):
+    return ta.ema(pd.Series(x), length=21).to_numpy()
+def RSI(x):
+    return ta.rsi(pd.Series(x), length=14).to_numpy()
+def MACDLine(x):
+    return ta.macd(pd.Series(x))['MACD_12_26_9'].to_numpy()
+def MACDHist(x):
+    return ta.macd(pd.Series(x))['MACDh_12_26_9'].to_numpy()
+def MACDSig(x):
+    return ta.macd(pd.Series(x))['MACDs_12_26_9'].to_numpy()
 
 
 class emaCross(Strategy):
 
     def init(self):
-        close_series = pd.Series(self.data.Close)
-        self.ema9 = self.I(lambda x: ta.ema(pd.Series(x), length=9).to_numpy(), self.data.Close)
-        self.ema21 = self.I(lambda x: ta.ema(pd.Series(x), length=21).to_numpy(), self.data.Close)
-        # self.ema21 = self.data.df['EMA21']
+        # Trend indicators
+        self.ema9 = self.I(EMA9, self.data.Close)
+        self.ema21 = self.I(EMA21, self.data.Close)
+
+        # Momentum indicators/oscilators
+        self.rsi = self.I(RSI, self.data.Close)
+        self.macd = self.I(MACDLine, self.data.Close) #macd has 3 signals 
+        self.macd_hist = self.I(MACDHist, self.data.Close)
+        self.macd_signal = self.I(MACDSig, self.data.Close)
 
     def next(self):
         # close old order and buy long
@@ -45,11 +56,24 @@ class emaCross(Strategy):
             stoploss = price * (1 + sl_pct)
             takeprofit = price * (1 - tp_pct)
             self.sell()
-            
-bt = Backtest(GOOG, emaCross, cash=1000, commission=.002)
+
+# Give user option to change file names
+path = input("Please enter file name (with csv): ")
+# "SPY_2024-01-01_2024-01-04.csv" --sample file
+aggData = pd.read_csv(path, parse_dates=['timestamp'])
+aggData.rename(columns={'open': 'Open', 'high': 'High', 'low': 'Low', 
+                       'close': 'Close', 'volume': 'Volume'}, inplace=True)
+aggData.set_index('timestamp', inplace=True)
+
+# for resampling data
+aggData = aggData.resample('2min').agg({
+    'Open': 'first',
+    'High': 'max',
+    'Low': 'min',
+    'Close': 'last',
+    'Volume': 'sum'
+}).dropna()
+
+bt = Backtest(aggData, emaCross, cash=1000, commission=.002)
 output = bt.run()
 bt.plot()
-print(output)
-# print(GOOG)
-        
-
