075343e0919eae717787a4abc664621a0a1820f4
Ashwin Kanitkar <ashwinkanitkar@outlook.com>
2025-07-25 02:26:28 +1000

    fix: run_all_files and orb code

--- src/run_all_files.py
+++ src/run_all_files.py
@@ -1,25 +1,25 @@
 import os
 import pandas as pd
 from datetime import datetime
-from strategies import ORB, master_file
+from strategies import breakout_orb15, master_file
 
 # path = input("Please enter file name (with csv): ")
 # path = "SPY_2025-03-09_2025-03-14.csv"
 # "SPY_2024-01-01_2024-01-04.csv" --sample file
-df = pd.read_csv('data\AMD\AMD_2025-06-01_2025-06-06.csv', parse_dates=True, index_col='timestamp')
+df = pd.read_csv('data\SPY\SPY_2024-01-17_2024-01-20.csv', parse_dates=True, index_col='timestamp')
 df.dropna(inplace=True)
 
 df.rename(columns={'open': 'Open', 'high': 'High', 'low': 'Low', 
                        'close': 'Close', 'volume': 'Volume'}, inplace=True)
 
 
-timestamp = datetime.now().strftime("%Y%m%d_%H%m")
+timestamp = datetime.now().strftime("%Y%m%d_%H%M")
 output_dir = f"outputs/results_{timestamp}"
 os.makedirs(output_dir, exist_ok=True)
 
 strategies = {
-    "EMA9/21": master_file.run,
-    "Bolinger": ORB.run
+    # "EMA9/21": master_file.run,
+    "ORB15": breakout_orb15.run
 }
 
 # for each name/fn in dict
--- src/strategies/breakout_orb15.py
+++ src/strategies/breakout_orb15.py
@@ -1,6 +1,4 @@
 from backtesting import Backtest, Strategy
-from backtesting.lib import crossover
-from backtesting.lib import resample_apply
 from datetime import time
 
 import numpy as np
@@ -9,109 +7,65 @@ import pandas_ta as ta # Using panda's TA lib
 from backtesting.test import GOOG #Google test data between 08/2004 -> 05/2009
 
 def run(df):
+
+    df = find_opening_range(df)
     bt = Backtest(df, orb15, cash=100000, commission=.002, trade_on_close=True)
     stats = bt.run()
     return bt, stats
 
+def find_opening_range(df):
+    df['date'] = df.index.date
+    df['ORB_high'] = None
+    df['ORB_low'] = None
+    for day, day_df in df.groupby('date'):
+        opening_range = day_df.between_time("9:30", "9:45")
+        orb_high = opening_range['High'].max()
+        orb_low = opening_range['Low'].min()
 
-# trend indicators
-def EMA9(x):
-    return ta.ema(pd.Series(x), length=9).to_numpy()
-def EMA21(x):
-    return ta.ema(pd.Series(x), length=21).to_numpy()
+        df.loc[day_df.index, 'ORB_high'] = orb_high
+        df.loc[day_df.index, 'ORB_low'] = orb_low
 
-# volatility indicators
-def ADX(High, Low, Close):
-    return ta.adx(pd.Series(High), pd.Series(Low), pd.Series(Close))
+    return df
 
-# volume indicators
-def VWAP(high, low, close, volume):
-    idx = VWAP.df_index[:len(close)]
-    h = pd.Series(high, index=idx)
-    l = pd.Series(low, index=idx)
-    c = pd.Series(close, index=idx)
-    v = pd.Series(volume, index=idx)
-
-    vwap_series = ta.vwap(high=h, low=l, close=c, volume=v)
-    return vwap_series.to_numpy()
-def DailyPOC(df):
-    df = df.copy()
-    df['Date'] = df.index.date
-    poc_list = []
-
-    for date, group in df.groupby('Date'):
-        # Use close prices or better: price buckets with volume sum
-        most_common_price = group['Close'].round(2).mode()
-        poc_value = most_common_price.iloc[0] if not most_common_price.empty else np.nan
-        poc_list.extend([poc_value] * len(group))
-
-    return np.array(poc_list)
-def POC_2hour(df):
-    df = df.copy()
-    df['TimeBin'] = df.index.floor('2H')  # Floor to the nearest 2-hour window
-    poc_list = []
-
-    for time_bin, group in df.groupby('TimeBin'):
-        most_common_price = group['Close'].round(2).mode()
-        poc_value = most_common_price.iloc[0] if not most_common_price.empty else np.nan
-        poc_list.extend([poc_value] * len(group))
-
-    return np.array(poc_list)
-
-def badTimeToTrade(current_time):
-    # Return True if NOT in the trading window
-    if time(9, 30) <= current_time <= time(13, 15):
-        return False  # Good time to trade
-    return True  # Outside trading window → skip
+def bad_to_trade(current_time):
+    if time(9,45) <= current_time <= time(12,00):
+        return False
+    return True
 
 # Opening Range Breakout Strategy which defines the range in the first 15min 
 # Style: Breakout + volatility
 class orb15(Strategy):
 
     def init(self):
-        # Trend indicators
-        self.ema9 = self.I(EMA9, self.data.Close)
-        self.ema21 = self.I(EMA21, self.data.Close)
-        
+
         # Momentum 
         self.atr = self.I(lambda h, l, c: ta.atr(pd.Series(h), pd.Series(l), pd.Series(c), length=14).to_numpy(), self.data.High, self.data.Low, self.data.Close)
-        
-        VWAP.df_index = self.data.df.index  # make sure your index is datetime!
-        self.vwap = self.I(VWAP, self.data.High, self.data.Low, self.data.Close, self.data.Volume)
-        self.poc = self.I(DailyPOC,self.data.df)
-        self.poc = self.I(POC_2hour,self.data.df)
-
-        self.adx = self.I(ADX, self.data.High, self.data.Low, self.data.Close)
+        self.last_traded_date = None
 
     def next(self):
-        # close old order and buy long
-        if pd.isna(self.ema9[-1]) or pd.isna(self.ema21[-1]):
-            return
-        # current_index = len(self.data.Close) - 1
-        # self.htfBullish = self.data.df['HTFSig'].iloc[current_index]
+        price = self.data.Close[-1]
         current_time = self.data.df.index[len(self.data.Close) - 1].time()
+        current_date = self.data.df.index[len(self.data.Close)-1].date()
+
+        if bad_to_trade(current_time):
+            if self.position:
+                self.position.close()
+            return
         
-        # if badTimeToTrade(current_time):
-        #     return  # Skip this candle
-        
-        current_adx = self.adx[-1]  # current ADX value
-        if current_adx < 20:
-        # skip or reduce position size because of weak trend
+        if self.last_traded_date == current_date:
             return
 
-        # Long
-        if crossover(self.ema9, self.ema21):
-            # if (self.htfBullish):
-                stoploss = self.data.Close[-1] - self.atr[-1]
-                takeprofit = self.data.Close[-1] + 2 * self.atr[-1]
-                self.buy(sl=stoploss, tp=takeprofit, size=1)
-                print("\n",f"Long Entered {current_time} with sl:{stoploss:.2f}, tp:{takeprofit:.2f}")
+        if self.data.ORB_high[-1] < price and not self.position:
+             sl = price - self.atr[-1]
+             tp = price + 2 * self.atr[-1]
+             self.buy(sl=sl, tp=tp, size=1)
+             self.last_traded_date = current_date
+        
+        if self.data.ORB_low[-1] > price and not self.position:
+             sl = price + self.atr[-1]
+             tp = price - 2 * self.atr[-1]
+             self.sell(sl=sl, tp=tp, size=1)
+             self.last_traded_date = current_date
+        
 
-        # Short
-        elif crossover(self.ema21, self.ema9):
-            # if not (self.htfBullish):
-                stoploss = self.data.Close[-1] + self.atr[-1]
-                takeprofit = self.data.Close[-1] - 2 * self.atr[-1]
-                print("\n",f"Short Entered {current_time} with sl:{stoploss:.2f}, tp:{takeprofit:.2f}")
-                self.sell(sl=stoploss, tp=takeprofit, size=1)
 
